876f64aac21be22effbf3bc47d9d315f8e917a65
Meah Tahmeed Ahmed <tahmeed121212@gmail.com>
2021-10-02 17:27:44 -0500

    tree classifier

---
+++ Decision/Scorer.py
@@ -0,0 +1,14 @@
+from sklearn import tree
+
+X = 
+Y = 
+
+classifier = tree.DecisionTreeClassifier()
+classifier.fit(X, Y)
+
+
+# CSV to python data table
+# Take each value from the python data table
+# Predict using on the Decision Tree
+# Add each predicted score to the python data table
+# Add python data table to CSV file
